5c35118ebbe65e6fdb14b319055f3a81abd67edb
Dimas Kiperman <126020343+kiper7777@users.noreply.github.com>
2025-01-30 17:56:22 +0000

    Add files via upload

---
+++ php300125/for_each.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+//do while
+
+$i=1;
+while($i<=12)
+{
+    echo "PHP lesson <br>";
+    $i+=2;
+}
+echo "Out of while loop <br>";
+
+
+// do while 1
+
+// $b=13;
+// do{
+//     echo "This is example of do while <br>";
+//     $b++;
+// }while($b<=12)
+
+// do while 2
+
+// $b=1;
+// do{
+//     echo "This is example of do while <br>";
+//     $b++;
+// }while($b>=8)
+
+
+//for each
+
+// $num=1;
+// while($num<=20)
+// {
+//     if($num%2==0)
+//     {
+//         echo "$num is even <br>";
+//     } 
+//     else {
+//         echo "$num is odd <br>";
+//     }
+//     $num++;
+// }
+
+?>
+</body>
+</html>
