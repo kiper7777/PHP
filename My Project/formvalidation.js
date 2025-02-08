3786830be266d7be038325bd475aaffa932c80e2
Dimas Kiperman <126020343+kiper7777@users.noreply.github.com>
2025-02-08 00:27:45 +0000

    Add files via upload

--- My Project/formvalidation.js
+++ My Project/formvalidation.js
@@ -6,22 +6,34 @@ document.getElementById("submit").addEventListener("click", (e)=>
     let password=document.getElementById('password').value;
     isvalid=true;
     console.log(email)
+    console.log(name)
+    console.log(password)
 
     let email_pattern=/^([a-zA-Z0-9 \ .-]+)@([a-zA-Z0-9 -]+).([a-z]{2,20})$/
     let name_pattern=/^[a-zA-Z0-9-._]{2,20}$/
+    let password_pattern=/^[a-zA-Z0-9]{4,6}$/
 
       let email_match=email_pattern.test(email)
       let name_match=name_pattern.test(name)
+      let password_match=password_pattern.test(password)
       console.log(email_match)
+      console.log(name_match)
+      console.log(password_match)
 
     if(!email_match)
         {
             document.getElementById("emailerror").innerHTML="Email is not valid";
             isvalid=false;
         }  
+
     if(!name_match)
         {
             document.getElementById("nameerror").innerHTML="Username is not valid";
             isvalid=false;
-        }    
+        }   
+    if(!password_match)
+        {
+            document.getElementById("passworderror").innerHTML="Password is not valid";
+            isvalid=false;
+        }     
 })
